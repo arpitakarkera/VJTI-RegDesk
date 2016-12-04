53d06775d627dfc162cbaf4b7740392df9021112
Arpita Karkera <karkeraarpita@gmail.com>
2016-12-04 16:37:35 +0530

    Add footer.php and update header and footer
    Wrote code(almost) for header and footer. Some things are left to fill.

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+		
+		<footer>
+			&copy; <?php echo date('Y'); ?>, RegDesk Inc.
+			<a href="developers.php">Meet the Devs!</a>
+		</footer>
+</body>
+</html>
--- view/header.php
+++ view/header.php
@@ -1,1 +1,26 @@
+<?php
+	/*
+	 * @author: Arpita Karkera
+	 * @date: 4th December, 2016
+	 * 
+	 * Header template file. To be included in every page.
+	 */
+?>
 
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Dynamically render the title. If not set just show "VJTI RegDesk". Value of title is set before including the header.-->
+	<title>VJTI RegDesk<?php if(isset($title)) echo " | ".$title; ?></title>
+</head>
+<body>
+<!--Display navigation bar for all pages except index page-->
+<?php
+	if (basename($_SERVER['PHP_SELF']) != 'index.php') {
+		echo '<header>';
+		// some other stuff
+		echo '</header>';
+	}
+?>
