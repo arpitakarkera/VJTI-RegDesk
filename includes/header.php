366a67abf6a01baccf9c272a7eea3fb32e977359
sunaina25 <punyanisunaina38@gmail.com>
2016-12-10 17:36:17 +0530

    index.php formatted ,header.phhp modified,images added

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
 	/*
-	 * @author: Arpita Karkera
+	 * @author:Sunaina Punyani, Arpita Karkera
 	 * @date: 4th December, 2016
 	 * 
 	 * Header template file. To be included in every page.
@@ -11,11 +11,56 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<!--Dynamically render the title. If not set just show "VJTI RegDesk". Value of title is set before including the header.-->
 	<title>VJTI-RegDesk<?php if(isset($title)) echo " | ".$title; ?></title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<?php
 		if (basename($_SERVER['PHP_SELF']) == 'index.php') {
-			// css link for index page
+			echo '<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Domine:700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet"> 
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <style>
+
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 100%;
+      margin:auto;
+  }
+ #p.container-fluid
+  {
+  	 background-image: url("images/index_background.jpg");
+    background-repeat: repeat;
+    background-position: right top;
+    background-attachment: fixed;
+  	
+  }
+  .transparent
+  {
+  	background-color:#FFFFFF;
+  	box-shadow:inset 0px 1px 0 rgba(0,0,0,.075);
+  }
+  .left-border-none
+  {
+  	
+  	box-shadow:inset 0px 1px 0 rgba(0,0,0,.075);
+  }
+   {
+  background: #000 !important;
+  color: #0f0 !important;
+  outline: solid #f00 1px !important;
+}
+  </style>';
+
 		}
 		else if (basename($_SERVER['PHP_SELF']) == 'signup.php') {
 			// for signup
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	/*
-	 * @author: Arpita Karkera
+	 * @author: Sunaina Punyani,Arpita Karkera
 	 * @date: 5th December, 2016
 	 * 
 	 * Index page.
@@ -22,23 +22,189 @@
 	require_once(__DIR__ . '/includes/header.php');
 ?>
 
-<div>
-	<p><?php echo $err_msg; ?></p>
-	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-		<input type="text" name="user_email" placeholder="e-mail" required value="<?php if(isset($user_email)) echo $user_email; ?>">
-		<br>
-		<input type="password" name="user_password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;" required>
-		<br>
-		<input type="submit" name="submit" value="Sign In">
-		<br>
-		<a href="public/forgot.php">Forgot?</a>
-		<p>
-			Don't have an account?
+<div class="container-fluid" id="p" style="min-height:100%;height:100%;width:100%">
+	<div class="row">
+		<div class="col-sm-8">
 			<br>
-			<a href="public/signup.php"><button type="button">Sign Up!</button></a>
+			
+				<img src="images/logo.png" class="img-circle img-responsive" alt="VJTI-RegDesk" width="200" height="200">
+				<p style="font-family:'Domine', serif;font-size:120px;color:#FFFFFF"> VJTI-RegDesk </p>
+				<p style="font-family: 'Droid Sans', sans-serif;font-size:40px;color:rgb(195,195,195)">One  Account . Many  Oppurtunities .</p>
+			
+
+
+
+		</div>
+		<div class="col-sm-4">
+		<br>
+		<br>
+		<br>
+		<p><?php echo $err_msg; ?></p>
+		<br>
+		<br>
+		<br>
+		
+		
+	<form roll="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+		
+
+		<div class="form-group">
+		<div class="input-group">
+		<span class="input-group-addon transparent"><span class="glyphicon glyphicon-envelope"></span></span>
+		<input type="email" class="form-control left-border-none" id="email" name="user_email" placeholder="e-mail" required value="<?php if(isset($user_email)) echo $user_email; ?>">
+		</div>
+		
+		</div>
+		
+		<br>
+		<div class="form-group">
+		<div class="input-group">
+		<span class="input-group-addon transparent"><span class="glyphicon glyphicon-lock"></span></span>
+		<input type="password" class="form-control" id="pwd" name="user_password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;" required>
+		</div>
+		</div>
+		
+		<div class="col-sm-offset-5 col-sm-7">
+		<button type="submit" class="btn btn-success btn-lg" name="submit" value="Sign In">Sign In!</button>
+		</div>
+		<br>
+		<br>
+		<br>
+		<div class="col-sm-offset-4 col-sm-8">
+		<a href="public/forgot.php" style="font-family:20px 'Montserrat', sans-serif; line-height: 1.8;font-size:20px">
+		<b>Forgot  Password?</b>
+		</a>
+		</div>
+		<br>
+		<br>
+		
+		<div class="col-sm-offset-2 col-sm-10">
+		<p style="font-family:'Montserrat', sans-serif;line-height: 1.8;font-size:25px;color:#FFFFFF">
+			<b>Don't  have  an  account?</b>
+
 		</p>
+		</div>
+		
+		<br>
+		<br>
+		<br>
+		<div class="col-sm-offset-5 col-sm-7">
+			<a href="public/signup.php"><button type="button"  class="btn btn-success btn-lg">Sign Up!</button></a>
+		</div>
 	</form>
+	    </div>
+	</div> 
+	<br>
+    <br>
+	<br> 
+	<br>
+	<br>
+	<br>  
 </div>
 
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container-fluid" >
+	<br>
+		<div id="mycarousel" class= "carousel slide"  data-ride="carousel">
+			<!.. Indicators ..>
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1" ></li>
+					<li data-target="#myCarousel" data-slide-to="2" ></li>
+					<li data-target="#myCarousel" data-slide-to="3" ></li>
+					<li data-target="#myCarousel" data-slide-to="4" ></li>
+					<li data-target="#myCarousel" data-slide-to="5" ></li>
+					<li data-target="#myCarousel" data-slide-to="6" ></li>
+					<li data-target="#myCarousel" data-slide-to="7" ></li>
+					<li data-target="#myCarousel" data-slide-to="8" ></li>
+
+				</ol>
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="images/coc.jpg" alt="COC" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/technovanza.jpg" alt="TECHNOVANZA" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/sra.jpg" alt="SRA" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/enthusia.jpg" alt="ENTHUSIA" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/e-cell.jpg" alt="E-CELL" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/dla.jpg" alt="DLA" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/pratibimb.jpg" alt="PRATIBIMB" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/rangawardhan.jpg" alt="RANGAWARDHAN" width="460" height="100">
+				</div>
+				<div class="item">
+					<img src="images/nirmaan.jpg" alt="NIRMAAN" width="460" height="100">
+				</div>
+			</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only" >Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only" >Next</span>
+		</a>
+	</div>
+</div>
+
+<script>
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+    $(".item5").click(function(){
+        $("#myCarousel").carousel(4);
+    });
+    $(".item6").click(function(){
+        $("#myCarousel").carousel(5);
+    });
+    $(".item7").click(function(){
+        $("#myCarousel").carousel(6);
+    });
+    $(".item8").click(function(){
+        $("#myCarousel").carousel(7);
+    });
+    $(".item9").click(function(){
+        $("#myCarousel").carousel(8);
+    });
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+</script>
 <!---Render footer-->
 <?php require_once(__DIR__ . '/includes/footer.php'); ?>
