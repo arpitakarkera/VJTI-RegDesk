bb4f188d84d9c91c17e9780fe9d976f835aa5634
apeksha98 <apekshagothawal56@gmail.com>
2017-03-31 07:13:34 +0530

    Merge branch 'master' into apeksha

--- includes/footer.php
+++ includes/footer.php
@@ -15,23 +15,24 @@
 	}
 ?>
 
-		<footer class="footer" style="background-image:url('<?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'images/background.jpg'; else echo '../images/background.jpg'; ?>'); background-repeat: repeat; background-position: left bottom; background-attachment: fixed;width:100% padding: 0px; border-top: 3px solid rgb(213,224,224);">
-			<div class="container-fluid" style="padding: 0px;">
-			<div class="row" style="">
+<footer class="footer" style="background-image:url('<?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'images/background.jpg'; else echo '../images/background.jpg'; ?>'); background-repeat: repeat; background-position: left bottom; background-attachment: fixed;width:100% padding: 0px; border-top: 3px solid rgb(213,224,224);">
+	<div class="container-fluid" style="padding: 0px;">
+		<div class="row" style="">
 			<div class="col-sm-4 text-left">
-			<p style="color:rgb(213,224,224); padding: 30px; vertical-align: center;">
-			&copy; <?php echo date('Y'); ?>, RegDesk Inc.
-			<br>
-			<?php echo USER; ?>
-			</p>
+				<p style="color:rgb(213,224,224); padding: 30px; vertical-align: center;">
+					&copy;
+					<?php echo date('Y'); ?>, RegDesk Inc.
+					<br>
+					<?php echo USER; ?>
+				</p>
 			</div>
 			<div class="col-sm-4">
 			</div>
 			<p class="col-sm-4 text-right" style="padding: 30px; vertical-align: center; font-size: 15px;">
-			<a href="developers.php">Meet the Devs!</a>
-			</div>
-			</div>
-			</div>
-		</footer>
+				<a href="developers.php">Meet the Devs!</a>
+		</div>
+	</div>
+</footer>
 </body>
+
 </html>
